ba692b26bc6ade21608ba534f71d6fad10aadd63
Alex Ponomarev <alexypon93@gmail.com>
2024-04-11 21:11:39 +0600

    product-properties

---
+++ src/components/producProperites.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function producProperites() {
+  return (
+    <div className='properties'>
+        <div className="product-length">
+            <span>Длина: </span>
+            <span></span>
+        </div>
+        <div className="product-width">
+            <span>Ширина: </span>
+            <span></span>
+        </div>
+        <div className="product-weight">
+            <span>Вес: </span>
+            <span></span>
+        </div>
+        <div className="product-height">
+            <span>Высота: </span>
+            <span></span>
+        </div>
+        <div className="product-color">
+            <span>Цвет: </span>
+            <span></span>
+        </div>
+
+    </div>
+  )
+}
