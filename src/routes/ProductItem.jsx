a18986742afa0e52c3728eec3fb97bf165eb7240
unknown <vadz71326@gmail.com>
2024-04-17 01:08:59 +0500

    slider task failed

--- src/routes/ProductItem.jsx
+++ src/routes/ProductItem.jsx
@@ -4,6 +4,8 @@ import './productItem.css'
 import BackLink from '../components/BackLink'
 import Slider1 from '../components/Slider1'
 import ProducProperites from '../components/producProperites'
+import Slider3 from '../components/Slider3'
+// import App from '../components/sliderTest'
 
 
 
@@ -20,11 +22,11 @@ export default function ProductItem() {
       <section className='product-item'>
         <ScrollRestoration />
         <div className="product-item-img">
-          <Slider1 item={item} link={link}></Slider1>
-          {/* <img src={link + item.mainImage} alt="" /> */}
-          {/* <div className="product-item-name" >
+          {/* <Slider1 item={item} link={link}></Slider1> */}
+          <img src={link + item.mainImage} alt="" />
+          <div className="product-item-name" >
             <span className=''>{item.name}</span>
-          </div> */}
+          </div>
         </div>
         <div className="container">
           <div className="product-item-wrapper">
@@ -36,11 +38,25 @@ export default function ProductItem() {
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere laboriosam dignissimos nemo quod explicabo repudiandae, praesentium aperiam labore, nulla a architecto. Assumenda molestias dolor facilis, explicabo laudantium illum, culpa repellendus repellat aliquam enim, aut quae voluptatibus tempore. Quia est, reiciendis repellendus numquam iste ab rerum ratione corporis inventore exercitationem incidunt ullam tempora, provident, pariatur eius et voluptas quae eaque magni ad officiis error maiores mollitia. Officia id ipsum quibusdam, illum atque ab, minima sed magnam quod distinctio sapiente velit cum, tempora quasi molestiae eveniet repellendus necessitatibus assumenda nostrum? Adipisci deleniti earum nesciunt eius quaerat temporibus voluptatibus est consequuntur voluptatem illo?</p>
               </div>
             </div>
-            <ProducProperites/>
+            </div>
+        </div>
+            <div className="product-item-img slider">
+          <Slider1 item={item} link={link}></Slider1>
+        </div>
+        <div className="product-item-img slider">
+          <Slider1 item={item} link={link}></Slider1>
+        </div>
+   
+        <div className="product-item-img slider small">
+        <Slider3 item={item} link={link}></Slider3>
+        </div>
+    
+        <div className="container">
+        <ProducProperites/>
             <a className="whatsapp-link" target='_blank' href={`https://wa.me/${phoneNumber}?text=${watsAppText}`}>
               <img src="/src/assets/images/WhatsAppButtonWhiteMedium.svg" alt="" />
             </a>
-          </div>
+          
         </div>
       </section>
 
