f05dd13083f46a140123d666b02044b2f532ae9d
unknown <vadz71326@gmail.com>
2024-06-05 00:27:47 +0500

    slider

--- src/components/AboutSlider.jsx
+++ src/components/AboutSlider.jsx
@@ -1,13 +1,14 @@
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide,  } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "./aboutSlider.css";
-
+// import "swiper/css/bundle";
+import 'swiper/css/pagination'
 // import required modules
-import { Navigation } from "swiper/modules";
+import { Navigation,Autoplay,Pagination } from "swiper/modules";
 import { nanoid } from "@reduxjs/toolkit";
 
 
@@ -21,8 +22,18 @@ AboutSlider({ aboutImages }) {
     <Swiper
       loop={true}
       rewind={true}
-      navigation={true}
-      modules={[Navigation]}
+      autoplay={{
+        delay: 2500 ,
+        disableOnInteraction: false,
+      }}
+      slidesPerView={1}
+            pagination={{
+              clickable: true,
+              dynamicMainBullets:true
+            }}
+      // navigation={true}
+      
+      modules={[Autoplay,Pagination]}
       className="mySwiper about"
     >
       {imagesToDisplay.map((el, index) => (
